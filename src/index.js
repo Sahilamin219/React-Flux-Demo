217bc61ce16fb3fb6a86a72a4353e6d4676ad277
Sahil Amin <48405411+Sahilamin219@users.noreply.github.com>
2020-12-27 20:11:04 +0530

    mock API to be added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+// create react app supports importing css
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -10,3 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   rootElement
 );
+// Why Mock API ?
+// start before real API exits,independance, Backup-plan,
+// ultra-fast, test-slowness, Aid-testing, Point to the real
+// API later.
