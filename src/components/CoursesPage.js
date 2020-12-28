a7874b357f808c1d9b4928478b5b3e82b4066356
Sahil Amin <48405411+Sahilamin219@users.noreply.github.com>
2020-12-28 15:00:34 +0530

    function hooks implemented

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi"; //this will call our mock api
 // to get list of all courses.
-class CoursePage extends React.Component {
+// class CoursePage extends React.Component {
+function CoursePage() {
+  const [courses, setCourses] = useState([]); //useState hook
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses), []);
+  });
   // constructor(props){
   //   super(props);//this ensures base classes constructor run first
   //   // after that happens we are ready to declare of state
@@ -10,49 +15,62 @@ class CoursePage extends React.Component {
   //   };
   // }
   // it works fine but we actually don't need a constructor to declare a class state
-  state = {
-    courses: []
-  }; //goal:request list of courses when this pages loads
+  // state = {
+  //   courses: []
+  // }; //goal:request list of courses when this pages loads
   // component must be mounted before we call setState
   // fetch is Promise based api and we use .then function to handle a response
   // Promises represent a future value.handle Async calls
-  componentDidMount() {
-    getCourses().then((courses) => this.setState({ courses: courses }));
-    // getCourses().then(function(courses){
-    // here we have refernce to the list of courses (we are ready to set the state)
-    // this.state.courses=courses;//don't so this
-    // this.setState({courses:courses});
-    // });
-  }
-  renderRow(courses) {
-    return (
-      <tr key={courses.id}>
-        <td>{courses.Title}</td>
-        <td>{courses.authorId}</td>
-        <td>{courses.category}</td>
-      </tr>
-    );
-  }
-  render() {
-    // render the lsit coursese in a table
-    return (
-      <>
-        <h2> All Coureses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>{this.state.courses.map(this.renderRow)}</tbody>
-        </table>
-      </>
-    );
-  }
+  // componentDidMount() {
+  // getCourses().then((courses) => this.setState({ courses: courses }));
+  // getCourses().then(function(courses){
+  // here we have refernce to the list of courses (we are ready to set the state)
+  // this.state.courses=courses;//don't so this
+  // this.setState({courses:courses});
+  // });
+  // }
+  // renderRow(courses) {
+  //   return (
+  //     <tr key={courses.id}>
+  //       <td>{courses.Title}</td>
+  //       <td>{courses.authorId}</td>
+  //       <td>{courses.category}</td>
+  //     </tr>
+  //   );
+  // }
+  // render() {
+  // render the lsit coursese in a table
+  return (
+    <>
+      <h2> All Coureses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((courses) => {
+            return (
+              <tr key={courses.id}>
+                <td>{courses.Title}</td>
+                <td>{courses.authorId}</td>
+                <td>{courses.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
+// }
 export default CoursePage;
 //{this.state.courses.map(courses => { })}
 // when creating multiple child component dynamically its important to
 // provide a key for each child
+// Hooks  - useState, useEffect, useContext
+//<tbody>{this.state.courses.map(this.renderRow)}</tbody>
+//<tbody>{courses.map(this.renderRow)}</tbody>
