fab4fbc0ead5310751f9bd14781313ea453f8d29
Sahil Amin <48405411+Sahilamin219@users.noreply.github.com>
2020-12-29 09:09:47 +0530

    Link with prompt done

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 const ManageCoursePage = (props) => {
   debugger;
   return (
     <>
       <h2>Manage Courses</h2>
+      <Prompt when={true} message="Are you sure you want to leave ?" />
       {props.match.params.slug}
     </>
   );
